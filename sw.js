189b14c93d7e7f03aec93fd3f57b76ff9ee01db2
aurea.wellness <virgy.piccolo@icloud.com>
2026-07-21 20:26:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aurea-v50';
+const CACHE='aurea-v51';
 const ASSETS=['index.html','app.html','manifest.webmanifest','icon-192.png','icon-512.png','icon-180.png','icon-512-maskable.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
